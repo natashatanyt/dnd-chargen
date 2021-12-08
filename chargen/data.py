b5cb2a68b1e4c5783e991e47ee7f78f9a2a96480
natashatanyt <natashantan.yt@gmail.com>
2021-12-08 18:15:02 +0800

    prints randomly chosen class, primary ability, saving throws, hit dice and hp

--- chargen/data.py
+++ chargen/data.py
@@ -4,93 +4,106 @@ classes = {
         'Weapons': ['Club', 'Dagger', 'Quarterstaff', 'Scimitar', 'Sickle', 'Shortspear', 'Sling', 'Longspear', 'Battleaxe', 'Greatsword', 'Greataxe', 'Warhammer', 'Lance', 'Longsowrd'],
         'Armour': ['Padded', 'Leather', 'Studded Leather', 'Hide', 'Chain Shirt', 'Scale mail', 'Breastplate', 'Half plate'],
         'Hit Dice': '1d12',
-        'HP': '',
+        'HP': '12',
+        'Saving Throws': 'Strength, Constituion',
     },
 
     'Bard': {
         'Stat': 'Charisma',
         'Weapons': ['Club', 'Longsword', 'Rapier', 'Shortbow', 'Dagger', 'Mace', 'Sickle', 'Quarterstaff', 'Spear', 'Crossbow, light', 'Dart', 'Sling'],
         'Armour': ['Padded', 'Leather', 'Studded Leather'],
-        'Hit Dice': '',
-        'HP': '',  
+        'Hit Dice': '1d8',
+        'HP': '8',  
+        'Saving Throws': 'Charisma, Dexterity',
     },
 
     'Cleric': {
         'Stat': 'Wisdom',
         'Weapons': ['Club', 'Longsword', 'Rapier', 'Shortbow', 'Dagger', 'Mace', 'Sickle', 'Quarterstaff', 'Spear', 'Crossbow, light', 'Dart', 'Sling'],
         'Armour': ['Padded', 'Leather', 'Studded Leather', 'Hide', 'Chain Shirt', 'Scale mail', 'Breastplate', 'Half plate'],
-        'Hit Dice': '',
-        'HP': '',
+        'Hit Dice': '1d8',
+        'HP': '8',
+        'Saving Throws': 'Wisdom, Charisma',
     },
 
     'Druid': {
         'Stat': 'Wisdom',
         'Weapons': ['Club', 'Dagger', 'Quarterstaff', 'Scimitar', 'Sickle', 'Shortspear', 'Sling', 'Longspear'],
         'Armour': ['Padded', 'Leather', 'Studded Leather'],
-        'Hit Dice': '',
-        'HP': '',
+        'Hit Dice': '1d8',
+        'HP': '8',
+        'Saving Throws': 'Wisdom, Intelligence',
     },
 
     'Fighter': {
         'Stat': 'Strength, Dexterity',
         'Weapons': ['Club', 'Dagger', 'Quarterstaff', 'Scimitar', 'Sickle', 'Shortspear', 'Sling', 'Longspear', 'Battleaxe', 'Greatsword', 'Greataxe', 'Warhammer', 'Lance', 'Longsowrd'],
         'Armour': ['Padded', 'Leather', 'Studded Leather', 'Hide', 'Chain Shirt', 'Scale mail', 'Breastplate', 'Half plate', 'Ring mail', 'Chain mail', 'Splint', 'Plate'],
-        'Hit Dice': '',
-        'HP': '',
+        'Hit Dice': '1d10',
+        'HP': '10',
+        'Saving Throws': 'Strength, Constitution',
     },
 
     'Monk': {
         'Stat': 'Wisdom, Dexterity',
         'Weapons': ['Club', 'Shortsword', 'Dagger', 'Handaxe', 'Javelin', 'Nunchaku', 'Quarterstaff', 'Sling'],
         'Armour': ['None'],
-        'Hit Dice': '',
-        'HP': '',
+        'Hit Dice': '1d8',
+        'HP': '8',
+        'Saving Throws': 'Dexterity, Strength',
     },
 
     'Paladin': {
         'Stat': 'Strength, Charisma',
         'Weapons': ['Club', 'Dagger', 'Quarterstaff', 'Scimitar', 'Sickle', 'Shortspear', 'Sling', 'Longspear', 'Battleaxe', 'Greatsword', 'Greataxe', 'Warhammer', 'Lance', 'Longsowrd'],
         'Armour': ['Padded', 'Leather', 'Studded Leather', 'Hide', 'Chain Shirt', 'Scale mail', 'Breastplate', 'Half plate', 'Ring mail', 'Chain mail', 'Splint', 'Plate'],
-        
+        'Hit Dice': '1d10',
+        'HP': '10',
+        'Saving Throws': 'Charisma, Wisdom',
     },
 
     'Ranger': {
         'Stat': 'Wisdom, Dexterity',
         'Weapons': ['Club', 'Dagger', 'Quarterstaff', 'Scimitar', 'Sickle', 'Shortspear', 'Sling', 'Longspear', 'Battleaxe', 'Greatsword', 'Greataxe', 'Warhammer', 'Lance', 'Longsowrd'],
         'Armour': ['Padded', 'Leather', 'Studded Leather', 'Hide', 'Chain Shirt', 'Scale mail', 'Breastplate', 'Half plate'],
-        'Hit Dice': '',
-        'HP': '',
+        'Hit Dice': '1d10',
+        'HP': '10',
+        'Saving Throws': 'Dexterity, Strength',
     },
 
     'Rogue': {
         'Stat': 'Dexterity',
         'Weapons': ['Crossbow, hand', 'Sap', 'Shortbow', 'Rapier', 'Sword, short', 'Club', 'Dagger', 'Javelin', 'Mace, light', 'Mace, heavy', 'Shortspear', 'Sickle', 'Spear', 'Gauntlet, spiked', 'Greatclub', 'Morningstar', 'Quarterstaff', 'Scythe', 'Sling', 'Crossbow, repeating light', 'Crossbow, repeating heavy'],
         'Armour': ['Padded', 'Leather', 'Studded Leather'],
-        'Hit Dice': '',
-        'HP': '',
+        'Hit Dice': '1d8',
+        'HP': '8',
+        'Saving Throws': 'Dexterity, Intelligence',
     },
 
     'Sorcerer': {
         'Stat': 'Constitution, Charisma',
         'Weapons': ['Dagger', 'Dart', 'Sling', 'Quarterstaff'],
         'Armour': ['None'],
-        'Hit Dice': '',
-        'HP': '',
+        'Hit Dice': '1d6',
+        'HP': '6',
+        'Saving Throws': 'Constitution, Charisma',
     },
 
     'Warlock': {
-        'Stat': 'Widsom, Charisma',
+        'Stat': 'Charisma, Wisdom',
         'Weapons': ['Club', 'Longsword', 'Rapier', 'Shortbow', 'Dagger', 'Mace', 'Sickle', 'Quarterstaff', 'Spear', 'Crossbow, light', 'Dart', 'Sling'],
         'Armour': ['Padded', 'Leather', 'Studded Leather'],
-        'Hit Dice': '',
-        'HP': '',
+        'Hit Dice': '1d8',
+        'HP': '8',
+        'Saving Throws': 'Charisma, Wisdom',
     },
 
     'Wizard': {
         'Stat': 'Intelligence',
         'Weapons': ['Dagger', 'Dart', 'Sling', 'Quarterstaff', 'Crossbow, light'],
         'Armour': ['None'],
-        'Hit Dice': '',
-        'HP': '',
+        'Hit Dice': '1d6',
+        'HP': '6',
+        'Saving Throws': 'Intelligence, Wisdom',
     }
 }
--- chargen/main.py
+++ chargen/main.py
@@ -1,6 +1,17 @@
 import random
+import data
 
-#ro
+#class to store character info
+class Character:
+    c_recStat = []
+    c_class = ""
+    c_weapon = []
+    c_armour = []
+    c_hp = 0
+
+
+
+#rolls stats 
 def RollStats():
     dice = []
 
@@ -16,9 +27,58 @@ def RollStats():
     
     return total
 
-stats = []
-for i in range(6):
-    stats.append(RollStats())
+def charCreation():
+    #stats
+    #creates an array and stores values for stats
+    stats = []
+    for i in range(6):
+        stats.append(RollStats())
+    #sorts stats in decreasing order and prints
+    stats.sort(reverse=True)
+    print("Stats:", *stats)
 
-stats.sort(reverse=True)
-print("Stats:", *stats)
+    #creates class to store character data
+    newChar = Character()
+
+    #chooses and prints class for the character
+    classes = []
+
+    for _class in data.classes:
+        classes.append(_class)
+
+    newChar.c_class = classes[random.randrange(0, len(classes))]
+    print ("Class:", newChar.c_class)
+
+    #depending on the class, the primary ability, hit dice, and HP are also printed
+    for type in data.classes:
+        if newChar.c_class == type:
+            print("Recommended Primary Ability:", data.classes[newChar.c_class]['Stat'])
+            print("Saving Throw Profiencies:", data.classes[newChar.c_class]['Saving Throws'])
+            print("Hit Dice:", data.classes[newChar.c_class]['Hit Dice'])
+            print("HP:", data.classes[newChar.c_class]['HP'], "+ Constition Modifier")
+            
+
+#main function
+user_input = ""
+
+while user_input != "n":
+    user_input = input("Create Character? (Y/N): ").lower()
+
+    if user_input == 'y':
+        charCreation()
+
+        #divider
+        print ("---------------------------")
+    
+    #ends char creation
+    elif user_input == 'n':
+        print("Ending Character Creation.")
+
+    #invalid input
+    else:
+        print("Invalid Input. Try Again.")
+
+
+
+
+
