33268d780f9d3328ad808fe9639c0cb8bb8fef53
natashatanyt <natashantan.yt@gmail.com>
2021-12-13 18:16:34 +0800

    added weapon + armour

--- chargen/main.py
+++ chargen/main.py
@@ -3,12 +3,7 @@ import data
 
 #class to store character info
 class Character:
-    c_recStat = []
     c_class = ""
-    c_weapon = []
-    c_armour = []
-    c_hp = 0
-
 
 
 #rolls stats 
@@ -27,16 +22,9 @@ def RollStats():
     
     return total
 
+#char creation fn
 def charCreation():
-    #stats
-    #creates an array and stores values for stats
-    stats = []
-    for i in range(6):
-        stats.append(RollStats())
-    #sorts stats in decreasing order and prints
-    stats.sort(reverse=True)
-    print("Stats:", *stats)
-
+    #class
     #creates class to store character data
     newChar = Character()
 
@@ -49,14 +37,39 @@ def charCreation():
     newChar.c_class = classes[random.randrange(0, len(classes))]
     print ("Class:", newChar.c_class)
 
+    #stats
+    #creates an array and stores values for stats
+    stats = []
+    for i in range(6):
+        stats.append(RollStats())
+    #sorts stats in decreasing order and prints
+    stats.sort(reverse=True)
+    print("Stats:", *stats)
+
     #depending on the class, the primary ability, hit dice, and HP are also printed
     for type in data.classes:
         if newChar.c_class == type:
             print("Recommended Primary Ability:", data.classes[newChar.c_class]['Stat'])
+            print("\n")
             print("Saving Throw Profiencies:", data.classes[newChar.c_class]['Saving Throws'])
             print("Hit Dice:", data.classes[newChar.c_class]['Hit Dice'])
             print("HP:", data.classes[newChar.c_class]['HP'], "+ Constition Modifier")
-            
+    
+    print("\n")
+
+    #prints suggested randomized weapon and armour
+    weapons = []
+    armour = []
+
+    for Weapons in data.classes[newChar.c_class]['Weapons']:
+        weapons.append(Weapons)
+
+    for Armour in data.classes[newChar.c_class]['Armour']:
+        armour.append(Armour)
+     
+    print("Weapon:", weapons[random.randrange(0, len(weapons))])
+    print("Armour:", armour[random.randrange(0, len(armour))])
+
 
 #main function
 user_input = ""
@@ -65,6 +78,8 @@ while user_input != "n":
     user_input = input("Create Character? (Y/N): ").lower()
 
     if user_input == 'y':
+        print ("---------------------------")
+
         charCreation()
 
         #divider
@@ -77,8 +92,3 @@ while user_input != "n":
     #invalid input
     else:
         print("Invalid Input. Try Again.")
-
-
-
-
-
